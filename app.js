038f2ccbb68b4b9c0a3f2af0cd3aecd751b109ca
mosrct <mosrutchata@gmail.com>
2022-04-25 18:12:03 +0700

    update

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
-const express = require("express")
-const chalk = require("chalk")
-const debug = require("debug")('app');
+const express = require("express");
+const chalk = require("chalk");
+const debug = require("debug")("app");
 const morgan = require("morgan");
 const path = require("path");
-const app = express()
-const port = 3000;
+const app = express();
+const PORT = process.env.PORT;
+const productRouter = express.Router();
 
 app.use(morgan("combined"));
-app.use(express.static(path.join(__dirname,"/public/")));
+app.use(express.static(path.join(__dirname, "/public/")));
 
-app.get("/", (req,res) =>{
-    res.send("Hello Mos");  
-})
-app.listen(port,()=>{
-    debug("Listening on Port "+ chalk.green(port));
-})
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+productRouter.route("/").get((req, res) => {
+  res.send("Hello Products");
+});
+
+app.use("/about", productRouter);
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    username: "Rutchata.H",
+    customers: ["Mos", "Joy", "Bass"],
+  });
+});
+app.listen(PORT, () => {
+  debug("Listening on PORT " + chalk.green(PORT));
+});
